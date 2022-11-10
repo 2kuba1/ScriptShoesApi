61b51f52aa2f57e68c6a6fbf4b9ff7c85553da6f
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-10 21:05:31 +0100

    Changed quries classes to records

--- src/Features/Shoes/Queries/GetAllShoes/GetAllShoesQuery.cs
+++ src/Features/Shoes/Queries/GetAllShoes/GetAllShoesQuery.cs
@@ -3,7 +3,7 @@ using ScriptShoesCQRS.Models.Shoes;
 
 namespace ScriptShoesCQRS.Features.Shoes.Queries.GetAllShoes;
 
-public class GetAllShoesQuery : IRequest<IEnumerable<GetAllShoesDto>>
+public record GetAllShoesQuery : IRequest<IEnumerable<GetAllShoesDto>>
 {
 
 }
