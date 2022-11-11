03c6806f2e7b81b893d481808bc8025a05ee3ecc
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-11 19:43:59 +0100

    Added fluent validation and authorize button on swagger

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,15 @@
 using System.Reflection;
+using System.Text;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ScriptShoesCQRS.Database;
+using ScriptShoesCQRS.Features.Users;
+using ScriptShoesCQRS.Features.Users.Commands.CreateUser;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,16 +18,46 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddDbContext<AppDbContext>(config  =>
 {
     config.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:connectionString"));
 });
+builder.Services.AddAuthorization();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddFluentValidation(fv => fv.AutomaticValidationEnabled = true);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "ScriptShoes API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        In = ParameterLocation.Header,
+        Description = "Enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
+builder.Services.AddScoped<IValidator<CreateUserCommand>, CreateUserCommandValidator>();
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,6 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
