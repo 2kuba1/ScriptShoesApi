a8d2eea9b3c142770fa74e2a90b613204a12fcb5
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-12 17:25:46 +0100

    Changed claim type nameIdentifier to Id

--- src/Services/UserContext/UserContextService.cs
+++ src/Services/UserContext/UserContextService.cs
@@ -14,5 +14,5 @@ public class UserContextService : IUserContextService
     public ClaimsPrincipal User => _httpContextAccessor.HttpContext?.User;
 
     public int? GetUserId =>
-        User is null ? null : (int?)int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+        User is null ? null : (int?)int.Parse(User.FindFirst(c => c.Type == "Id").Value);
 }
