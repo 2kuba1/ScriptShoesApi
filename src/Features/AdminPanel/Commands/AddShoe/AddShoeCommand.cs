b09e5f6f74b7b29ec2e49b488e89213659e9fd2e
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-16 13:56:26 +0100

    Removed main img from add shoe command

--- src/Features/AdminPanel/Commands/AddShoe/AddShoeCommand.cs
+++ src/Features/AdminPanel/Commands/AddShoe/AddShoeCommand.cs
@@ -7,7 +7,6 @@ public record AddShoeCommand : IRequest<int>
     public string Brand { get; set; }
     public string Name { get; set; }
     public double CurrentPrice { get; set; }
-    public string MainImg { get; set; }
     public string ShoeType { get; set; }
 
     public string SizesList { get; set; }
