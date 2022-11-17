8d0dbe3998e102c871869ffda94ee172dc8da88b
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-17 23:32:50 +0100

    Add remove like endpoint

--- src/Controllers/ReviewsController.cs
+++ src/Controllers/ReviewsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using ScriptShoesApi.Entities;
 using ScriptShoesCQRS.Features.Reviews.Commands.AddLikeToReview;
 using ScriptShoesCQRS.Features.Reviews.Commands.CreateReview;
-using ScriptShoesCQRS.Features.Reviews.Queries;
+using ScriptShoesCQRS.Features.Reviews.Commands.RemoveLike;
 using ScriptShoesCQRS.Features.Reviews.Queries.GetLikedReviews;
 using ScriptShoesCQRS.Features.Reviews.Queries.GetReviewsStats;
 using ScriptShoesCQRS.Models.Reviews;
@@ -69,7 +69,21 @@ public class ReviewsController : ControllerBase
     [Route("addLikeToReview/{reviewId:int}")]
     public async Task<ActionResult> AddLikeToReview([FromRoute] int shoeId, [FromRoute] int reviewId)
     {
-        var result = await _mediator.Send(new AddLikeToReviewCommand()
+        await _mediator.Send(new AddLikeToReviewCommand()
+        {
+            ReviewId = reviewId,
+            ShoeId = shoeId
+        });
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "User,Admin")]
+    [Route("removeLikeFromReview/{reviewId:int}")]
+    public async Task<ActionResult> RemoveLikeFromReview([FromRoute] int shoeId, [FromRoute] int reviewId)
+    {
+        await _mediator.Send(new RemoveLikeCommand()
         {
             ReviewId = reviewId,
             ShoeId = shoeId
---
+++ src/Features/Reviews/Commands/RemoveLike/RemoveLikeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ScriptShoesCQRS.Features.Reviews.Commands.RemoveLike;
+
+public class RemoveLikeCommand : IRequest
+{
+    public int ShoeId { get; set; }
+    public int ReviewId { get; set; }
+}
