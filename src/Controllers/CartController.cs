a953f354139b8edefede84ccb2129c08f2234a60
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-18 20:27:22 +0100

    Add add to cart funcionality

---
+++ src/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ScriptShoesCQRS.Features.Cart.Commands.AddToCart;
+
+namespace ScriptShoesCQRS.Controllers;
+
+[ApiController]
+[Authorize(Roles = "User,Admin")]
+[Route("/api/cart")]
+public class CartController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CartController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Route("addToCart")]
+    public async Task<ActionResult> AddToCart([FromRoute] int shoeId)
+    {
+        await _mediator.Send(new AddToCartCommand()
+        {
+            ShoeId = shoeId
+        });
+
+        return NoContent();
+    }
+}
