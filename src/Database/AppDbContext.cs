faa4a9de187d334b9b0f198c6960a95584bacf64
2kuba1 <jakub.wojtyna06@gmail.com>
2022-11-18 20:19:37 +0100

    Add cart entity

--- src/Database/AppDbContext.cs
+++ src/Database/AppDbContext.cs
@@ -19,6 +19,8 @@ public class AppDbContext : DbContext
     public DbSet<ShoeSizes> ShoeSizes { get; set; }
     public DbSet<Reviews> Reviews { get; set; }
     public DbSet<ReviewsLikes> ReviewsLikes { get; set; }
+    public DbSet<Cart> Cart { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Roles>()
